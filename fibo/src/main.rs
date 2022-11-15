3afd9edb1e18db161e1053d846fb26aa1f5b1245
edwin <edwin.abel.3@gmail.com>
2022-11-16 00:18:25 +0800

    齐活.

--- fibo/src/main.rs
+++ fibo/src/main.rs
@@ -39,9 +39,59 @@ fn main() {
             &k.unwrap(),
             &_n.unwrap()
         );
+
+        if _display_all_number {
+            for index in 0.._n.unwrap() {
+                let fibo_param = FiboParam {
+                    _display_all_number,
+                    k: k.unwrap(),
+                    n: index,
+                };
+                print!("{} ", &fn_fibo(fibo_param));
+            }
+        } else {
+            let fibo_param = FiboParam {
+                _display_all_number,
+                k: k.unwrap(),
+                n: _n.unwrap(),
+            };
+            println!("value:{}", &fn_fibo(fibo_param));
+        }
     }
 }
 
 fn output_info() {
     println!("fibo [-a] k n")
+}
+
+struct FiboParam {
+    _display_all_number: bool,
+    k: u32,
+    n: u32,
+}
+
+fn fn_fibo(fibo: FiboParam) -> u32 {
+    match fibo.n.cmp(&fibo.k) {
+        std::cmp::Ordering::Less => 0,
+        std::cmp::Ordering::Equal => 1,
+        std::cmp::Ordering::Greater => {
+            if fibo.n == fibo.k {
+                1
+            } else if fibo.n == fibo.k + 1 {
+                1
+            } else {
+                let mut ret = 0;
+                for index in (fibo.n - fibo.k)..fibo.n {
+                    ret = ret
+                        + fn_fibo(FiboParam {
+                            _display_all_number: fibo._display_all_number,
+                            k: fibo.k,
+                            n: index,
+                        })
+                }
+
+                ret
+            }
+        }
+    }
 }
